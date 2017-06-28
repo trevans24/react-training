1a093a38c40d3909ef234040695c2cb293b71137
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-06-28 11:52:05 -0600

    figuring out props and map/filter in react

--- app/index.js
+++ app/index.js
@@ -12,9 +12,53 @@ require('./index.css');
 // component using Class that extends a parent component/object
 class App extends React.Component {
 	render() {
+		let friends = [
+		{
+			name: 'Bob',
+			friend: true
+		},
+		{
+			name: 'Alex',
+			friend: true
+		},
+		{
+			name: 'Jacy',
+			friend: true
+		},
+		{
+			name: 'Casey',
+			friend: false
+		},
+		{
+			name: 'Billy',
+			friend: false
+		}
+		];
 		return(
 			<div>
 				Hello!
+				<h1>People I know</h1>
+				<ul>
+					{friends.map((friend) => {
+						return <li key={friend.name}> {friend.name} </li>
+					})}
+				</ul>
+				<h1>Friends</h1>
+				<ul>
+					{friends.filter((user) => {
+						return user.friend === true;
+					}).map((user) => {
+						return <li key={user.name}> {user.name} </li>
+					})}
+				</ul>
+				<h1>Not Friends</h1>
+				<ul>
+					{friends.filter((user) => {
+						return user.friend !== true;
+					}).map((user) => {
+						return <li key={user.name}> {user.name} </li>
+					})}
+				</ul>
 			</div>
 		);
 	}
